5e95d0e2c503021815d708a0150d39026d8713ec
HS6986 <hiroaki.sato.private@gmail.com>
2025-04-29 16:51:53 +0900

    Fix indentation issues

--- model/modelmorphology.cpp
+++ model/modelmorphology.cpp
@@ -30,10 +30,10 @@ void ModelMorphology::init(const char *model_name, string model_params, StateFre
 		}
         num_params = 0;
     } else if (name == "GTR" || name == "GTRX") {
-				if (!(num_states <= 6 && phylo_tree->aln->getNPattern() >= 100)) {
-						outWarning("GTRX multistate model will estimate " + convertIntToString(getNumRateEntries()-1) + " substitution rates that might be overfitting for the length of your alignment/partition!");
-						outWarning("Please only use GTRX with large data and always test for model fit!");
-				}
+			if (!(num_states <= 6 && phylo_tree->aln->getNPattern() >= 100)) {
+				outWarning("GTRX multistate model will estimate " + convertIntToString(getNumRateEntries()-1) + " substitution rates that might be overfitting for the length of your alignment/partition!");
+				outWarning("Please only use GTRX with large data and always test for model fit!");
+			}
 	} else {
 		// if name does not match, read the user-defined model
 		readParameters(model_name);
